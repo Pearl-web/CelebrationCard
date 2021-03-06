f5f2053382497f350279a580eb8920c634d65823
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-03-06 16:28:41 +0000

    add styling and submit function to return at the bottom

--- static/index.js
+++ static/index.js
@@ -1,5 +1,26 @@
-document.getElementById('submit').addEventListener("click", changeColor)
 
-function changeColor(){
-    document.getElementById('submit').style.backgroundColor = "pink"
-;}
+
+const form = document.getElementById('form');
+const card = document.getElementById('card');
+const message = document.getElementById("message")
+const greeting= document.getElementById("greeting")
+
+
+
+
+
+form.addEventListener('submit', logSubmit);
+
+
+
+
+function logSubmit(event) {
+    event.preventDefault();
+    card.innerHTML = `${greeting.value} <br> ${message.value}`;
+
+  }
+
+
+
+
+
