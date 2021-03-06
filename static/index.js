97819f77930f3690566620beaba30de35c3c428f
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-03-06 16:45:29 +0000

    make card display on submit

--- static/index.js
+++ static/index.js
@@ -4,19 +4,23 @@ const form = document.getElementById('form');
 const card = document.getElementById('card');
 const message = document.getElementById("message")
 const greeting= document.getElementById("greeting")
+const header = document.querySelector("h1")
 
 
 
 
 
-form.addEventListener('submit', logSubmit);
+form.addEventListener('submit', createCard);
 
 
 
 
-function logSubmit(event) {
+function createCard(event) {
     event.preventDefault();
+    card.classList.add('styling')
     card.innerHTML = `${greeting.value} <br> ${message.value}`;
+    form.remove()
+    header.remove()
 
   }
 
